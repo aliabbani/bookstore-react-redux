3404c20a5a86fe62c66b6473b190f7f2499aefe5
ali abbani <aliox@ahlam.com>
2021-09-29 17:19:49 +0300

    Create reducer switch function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,27 @@
-// const ADD_BOOK = 'bookStore/books/ADD-BOOK';
-// const REMOVE_BOOK = 'bookStore/books/REMOVE-BOOK';
+const ADD_BOOK = 'bookStore/books/ADD-BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE-BOOK';
 
-// const initialState = [];
+const initialState = [];
 
-// export const addBook = (payload) => ({
-//   type: ADD_BOOK,
-//   payload,
-// });
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
 
-// export const removeBook = (payload) => ({
-//   type: REMOVE_BOOK,
-//   payload,
-// });
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
 
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ADD_BOOK:
-//       return [...state, action.payload];
-//     case REMOVE_BOOK:
-//       return state.filter((book) => book.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
 
-// export default reducer;
+export default reducer;
