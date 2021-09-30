5c56f2ad12e97a06886aca73c1df67a7f575a2ff
ali abbani <aliox@ahlam.com>
2021-09-30 20:42:46 +0300

    Create a separate navabar css file and import it inside navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
+import './Navbar.css';
 
 const Navbar = () => {
   const links = [
